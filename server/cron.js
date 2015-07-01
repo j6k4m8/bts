7720c1aab31e78de799585bfdd73d425c0b200c8
Jordan <j6k4m8@gmail.com>
2015-07-01 19:28:46 -0400

    Possibly done...

--- server/cron.js
+++ server/cron.js
@@ -1,11 +1,21 @@
 SyncedCron.add({
-  name: 'Crunch some important numbers for the marketing department',
-  schedule: function(parser) {
-    // parser is a later.parse object
-    return parser.text('every 2 hours');
-  },
-  job: function() {
-    var numbersCrunched = CrushSomeNumbers();
-    return numbersCrunched;
-  }
+    name: 'Get latest Actual from API',
+    schedule: function(parser) {
+        // TODO: Exclude holidays
+        return parser.text('at 4:01pm every weekday');
+    },
+    job: function() {
+        var lastTrade = YahooFinance.snapshot({symbols:['^GSPC'],fields: ['l1', 'd1']});
+        var closingSP = parseFloat(lastTrade['^GSPC'].lastTradePriceOnly);
+
+        if (!Actuals.findOne({
+            date: moment(lastTrade['^GSPC'].lastTradeDate).startOf('day').toDate()
+        })) {
+            Actuals.insert({
+                'value': closingSP,
+                'date': moment(lastTrade['^GSPC'].lastTradeDate).startOf('day').toDate(),
+                'change': (closingSP - lastActual().value) / lastActual().value
+            });
+        }
+    }
 });
